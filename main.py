3c7b65417237cd7d147b57cf5d779bd4f3f10140
LeonHecht <l-e-o-n-h@web.de>
2025-12-04 13:08:12 -0600

    turn retriever into a class

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-from model import Retriever
+from retriever import Retriever
+from data import Data
+import os
+from config import STORAGE_DIR
 
-retriever = Retriever(qtype="rewrite", corpus="clapnq")
-top_docs = retriever.retrieve(query="What is Tesla?", top_k=5)
+
+def get_bge_m3_model(checkpoint):
+    """ Load BAAI embeddings model."""
+    from FlagEmbedding import BGEM3FlagModel
+    print("Loading BAAI embeddings model from checkpoint:", checkpoint)
+    # model = BGEM3FlagModel(checkpoint, use_fp16=True) # Setting use_fp16 to True speeds up computation with a slight performance degradation
+    model = BGEM3FlagModel(checkpoint) # Setting use_fp16 to True speeds up computation with a slight performance degradation
+    return model
+
+
+qtype = "rewrite"
+corpus = "clapnq"
+
+data = Data(qtype, corpus)
+
+model = get_bge_m3_model('BAAI/bge-m3')
+emb_path = os.path.join(STORAGE_DIR, f'mtrag/data/corpus/embeddings/corpus_{corpus}_bge-m3-512.npy')
+
+retriever = Retriever(model, doc_ids=data.doc_ids, docs=data.docs, emb_path=emb_path)
+top_docs = retriever.retrieve_top_k(query="What is Tesla?", top_k=5)
 
 print(top_docs)
--- retriever.py
+++ retriever.py
@@ -10,148 +10,142 @@ MAX_QUERY_LEN = 48
 MAX_DOC_LEN = 512
 
 
-def compute_similarity(embeddings_queries, embeddings_docs, sim_type='dot'):
-    """
-    Given query and document embeddings, compute the similarity between queries and documents
-    and return the similarity matrix.
-    Dot-product and cosine similarity are supported.
+class Retriever:
+    def __init__(self, model, doc_ids, docs, emb_path) -> None:
+        self.model = model
+        self.doc_ids = doc_ids
+        self.docs = docs
+        self.doc_dict = dict(zip(doc_ids, docs))
+        self.embed_bge(docs, reuse_embeddings=True, path=emb_path)
 
-    Parameters
-    ----------
-    embeddings_queries : torch.Tensor
-        Tensor of query embeddings. Can be either 2D [num_queries, emb_dim] or 1D [emb_dim].
-    embeddings_docs : torch.Tensor
-        Tensor of document embeddings of shape [num_docs, emb_dim].
-    sim_type : str, optional
-        Type of similarity to use. Options: 'dot', 'cosine'. (default is 'dot').
+    def compute_similarity(self, embeddings_queries, embeddings_docs, sim_type='dot'):
+        """
+        Given query and document embeddings, compute the similarity between queries and documents
+        and return the similarity matrix.
+        Dot-product and cosine similarity are supported.
 
-    Returns
-    -------
-    torch.Tensor
-        Similarity matrix of shape [num_queries, num_docs].
-    """
-    print("Computing similarity...", end="")
+        Parameters
+        ----------
+        embeddings_queries : torch.Tensor
+            Tensor of query embeddings. Can be either 2D [num_queries, emb_dim] or 1D [emb_dim].
+        embeddings_docs : torch.Tensor
+            Tensor of document embeddings of shape [num_docs, emb_dim].
+        sim_type : str, optional
+            Type of similarity to use. Options: 'dot', 'cosine'. (default is 'dot').
 
-    # If there is just one query, unsqueeze to make it 2D
-    if embeddings_queries.dim() == 1:
-        embeddings_queries = embeddings_queries.unsqueeze(0)
+        Returns
+        -------
+        torch.Tensor
+            Similarity matrix of shape [num_queries, num_docs].
+        """
+        print("Computing similarity...", end="")
 
-    if sim_type == 'dot':
-        similarities = []
-        batch_size = 32
-        for query_batch in torch.split(embeddings_queries, batch_size, dim=0):
-            sim_batch = query_batch @ embeddings_docs.T
-            similarities.append(sim_batch)
-        similarity = torch.cat(similarities, dim=0)
-    elif sim_type == 'cosine':
-        # Normalize embeddings along the embedding dimension
-        embeddings_queries = F.normalize(embeddings_queries, p=2, dim=1)
-        embeddings_docs = F.normalize(embeddings_docs, p=2, dim=1)
-        similarity = (embeddings_queries @ embeddings_docs.T) * 100
-    else:
-        raise ValueError(f"Invalid similarity type: {sim_type}")
-    
-    print("Done.")
-    return similarity
+        # If there is just one query, unsqueeze to make it 2D
+        if embeddings_queries.dim() == 1:
+            embeddings_queries = embeddings_queries.unsqueeze(0)
 
-
-def retrieve(query_ids, doc_ids, similarity, top_k=None):
-    """
-    Build a run dictionary:
-        {
-            query_id: {doc_id: score, ...}
-        }
-
-    Parameters
-    ----------
-    query_ids : list
-    doc_ids : list
-    similarity : 2D array-like where similarity[i][j] is score for query i and doc j
-    top_k : int or None
-        If set, return only the top_k most similar docs per query.
-
-    """
-    run = {}
-    for i in tqdm(range(len(query_ids)), desc="Creating run"):
-        query_sim = similarity[i]
-
-        # Zip doc_ids and scores
-        docs_scores = list(zip(doc_ids, query_sim))
-
-        # Sort descending by similarity
-        docs_scores.sort(key=lambda x: x[1], reverse=True)
-
-        # Keep top_k if specified
-        if top_k:
-            docs_scores = docs_scores[:top_k]
-
-        # Build the inner dict
-        run[str(query_ids[i])] = {str(doc_id): float(score) for doc_id, score in docs_scores}
-
-    print("Run created.")
-    return run
-
-
-def embed_bge(model, docs, queries, doc_ids, query_ids, reuse_embeddings, path):
-    """
-    Embed the queries and documents using the BAAI embeddings models and compute the similarity between queries and documents.
-    Calls the retrieve function.
-
-    Args:
-        model: BAAI embeddings model.
-        docs (dict): Dictionary with document_id as key and text as value.
-        queries (list): List of queries.
-        top_k (int): Number of most similar documents to retrieve.
-
-    Returns:
-        dict: Dictionary with query as key and a list of tuples of (similarity, document text, doc_id) as value.
-    """
-    embeddings_queries = model.encode(queries, batch_size=8, max_length=MAX_QUERY_LEN)['dense_vecs']
-    # Embed entire corpus if file does not exist
-    if not os.path.exists(path) or not reuse_embeddings:
-        print("Embedding docs...", end="")
-        embeddings_docs = model.encode(docs, batch_size=64, max_length=MAX_DOC_LEN)['dense_vecs']
-        print("Done.")
+        if sim_type == 'dot':
+            similarities = []
+            batch_size = 32
+            for query_batch in torch.split(embeddings_queries, batch_size, dim=0):
+                sim_batch = query_batch @ embeddings_docs.T
+                similarities.append(sim_batch)
+            similarity = torch.cat(similarities, dim=0)
+        elif sim_type == 'cosine':
+            # Normalize embeddings along the embedding dimension
+            embeddings_queries = F.normalize(embeddings_queries, p=2, dim=1)
+            embeddings_docs = F.normalize(embeddings_docs, p=2, dim=1)
+            similarity = (embeddings_queries @ embeddings_docs.T) * 100
+        else:
+            raise ValueError(f"Invalid similarity type: {sim_type}")
         
-        try:
-            # save embeddings
-            print("Saving embeddings...", end="")
-            np.save(path, embeddings_docs)
-            print("Done.")
-        except:
-            print("Could not save embeddings.")
-    else:
-        # Load embeddings
-        embeddings_docs = np.load(path)
+        print("Done.")
+        return similarity
 
-    # Compute similarities
-    similarity = compute_similarity(torch.tensor(embeddings_queries, dtype=torch.float32), torch.tensor(embeddings_docs, dtype=torch.float32))
-    run = retrieve(query_ids, doc_ids, similarity)
-    return run
+    def _retrieve(self, query_ids, similarity, top_k=None):
+        """
+        Build a run dictionary:
+            {
+                query_id: {doc_id: score, ...}
+            }
 
-def retrieve_top_k(model, query, doc_ids, path, top_k=5):
-    """
-    Embed the queries and documents using the BAAI embeddings models and compute the similarity between queries and documents.
-    Calls the retrieve function.
+        Parameters
+        ----------
+        query_ids : list
+        similarity : 2D array-like where similarity[i][j] is score for query i and doc j
+        top_k : int or None
+            If set, return only the top_k most similar docs per query.
+        """
+        run = {}
+        for i in tqdm(range(len(query_ids)), desc="Creating run"):
+            query_sim = similarity[i]
 
-    Args:
-        model: BAAI embeddings model.
-        docs (dict): Dictionary with document_id as key and text as value.
-        queries (list): List of queries.
-        top_k (int): Number of most similar documents to retrieve.
+            # Zip doc_ids and scores
+            docs_scores = list(zip(self.doc_ids, query_sim))
 
-    Returns:
-        dict: Dictionary with query_id as key and a dict of (doc_id, similarity) as value.
-    """
-    embedding_query = model.encode(query, max_length=MAX_QUERY_LEN)['dense_vecs']
+            # Sort descending by similarity
+            docs_scores.sort(key=lambda x: x[1], reverse=True)
 
-    embeddings_docs = np.load(path)
+            # Keep top_k if specified
+            if top_k:
+                docs_scores = docs_scores[:top_k]
 
-    # Compute similarities
-    similarity = compute_similarity(torch.tensor(embedding_query, dtype=torch.float32), torch.tensor(embeddings_docs, dtype=torch.float32))
-    
-    run = retrieve(['qid_1'], doc_ids, similarity, top_k)['qid_1']
+            # Build the inner dict
+            run[str(query_ids[i])] = {str(doc_id): float(score) for doc_id, score in docs_scores}
 
-    top_doc_ids = [id for id, score in run.items()]
+        print("Run created.")
+        return run
 
-    return top_doc_ids
+    def embed_bge(self, texts, reuse_embeddings, path):
+        """
+        Embed the queries and documents using the BAAI embeddings models and compute the similarity between queries and documents.
+        Calls the retrieve function.
+
+        Args:
+            docs (dict): Dictionary with document_id as key and text as value.
+            queries (list): List of queries.
+            top_k (int): Number of most similar documents to retrieve.
+
+        Returns:
+            dict: Dictionary with query as key and a list of tuples of (similarity, document text, doc_id) as value.
+        """
+        if not os.path.exists(path) or not reuse_embeddings:
+            self.embeddings = self.model.encode(texts, batch_size=64, max_length=MAX_DOC_LEN)['dense_vecs']
+            
+            try:
+                # save embeddings
+                print("Saving embeddings...", end="")
+                np.save(path, self.embeddings)
+                print("Done.")
+            except:
+                print("Could not save embeddings.")
+        else:
+            # Load embeddings
+            self.embeddings = np.load(path)
+
+    def retrieve_top_k(self, query, top_k=5):
+        """
+        Embed the queries and documents using the BAAI embeddings models and compute the similarity between queries and documents.
+        Calls the retrieve function.
+
+        Args:
+            model: BAAI embeddings model.
+            docs (dict): Dictionary with document_id as key and text as value.
+            queries (list): List of queries.
+            top_k (int): Number of most similar documents to retrieve.
+
+        Returns:
+            dict: Dictionary with query_id as key and a dict of (doc_id, similarity) as value.
+        """
+        embedding_query = self.model.encode(query, max_length=MAX_QUERY_LEN)['dense_vecs']
+        print(embedding_query)
+
+        # Compute similarities
+        similarity = self.compute_similarity(torch.tensor(embedding_query, dtype=torch.float32), torch.tensor(self.embeddings, dtype=torch.float32))
+        
+        run = self._retrieve(['qid_1'], similarity, top_k)['qid_1']
+
+        top_doc_ids = [id for id, score in run.items()]
+
+        top_docs = [self.doc_dict[id] for id in top_doc_ids]
+        return top_docs
